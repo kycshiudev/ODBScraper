efbb4313095af415bacb4e2d27cad637230c0bcc
Kenny Shiu <kycshiudev@gmail.com>
2013-12-14 16:46:09 -0800

    jsoup example and exploration. use maven later, import statements may change

--- ODBScrapper.java
+++ ODBScrapper.java
@@ -1,8 +1,17 @@
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+
 public class ODBScrapper{
 
 	public static void main (String[] args)
 	{
+		String html = "<html><head><title>First parse</title></head>"
+				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
+		Document doc = Jsoup.parse(html);
 		System.out.println("Hello World");
+		for (Node child:doc.childNodes()){
+			System.out.println(child.toString());
+		}
 	}
 	
 }
