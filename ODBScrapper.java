8f8a5fa199c528f2ffb7a68eb2ef5a45d1c4eb32
kycshiudev <kycshiudev@gmail.com>
2013-12-15 18:38:04 -0800

    More tree traversal of doc. Example for grabbing HTML from URL string

--- ODBScrapper.java
+++ ODBScrapper.java
@@ -5,13 +5,35 @@ public class ODBScrapper{
 
 	public static void main (String[] args)
 	{
-		String html = "<html><head><title>First parse</title></head>"
+		// jsoup sample.  parse html from string
+        	// and read out some nodes from the tree
+		String html1 = "<html><head><title>First parse</title></head>"
 				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
+        String html2 = "<html><head><title>First parse</title></head>"
+				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
+        String html = html1;
 		Document doc = Jsoup.parse(html);
 		System.out.println("Hello World");
-		for (Node child:doc.childNodes()){
-			System.out.println(child.toString());
-		}
+		//for (Node child:doc.childNodes()){
+		//	System.out.println(child.toString());
+		//}
+        System.out.println(doc.childNode(0).toString());
+        System.out.println(doc.childNode(0).childNodes().size());
+        System.out.println(doc.childNode(0).childNode(1).toString());
+        
+       	// Get HTML from string URL
+       	String URLstr = "http://www-inst.eecs.berkeley.edu/~cs164/fa13/";
+       	Document doc2;
+        
+       	try{
+           	doc2 = Jsoup.connect(URLstr).get();
+           	System.out.println(doc2.childNode(0).toString());
+        }
+        catch (Exception e)
+        {
+            System.out.println("couldn't connect to " + URLstr);
+        }
+        
 	}
 	
 }
