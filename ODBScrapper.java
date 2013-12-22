3744cff54a758ecbd6022976b6b3c2af65ddbe8c
Kenny Shiu <kycshiudev@gmail.com>
2013-12-22 14:44:21 -0800

    refactor examples

--- ODBScrapper.java
+++ ODBScrapper.java
@@ -1,28 +1,32 @@
 import org.jsoup.*;
 import org.jsoup.nodes.*;
+import java.text.DateFormat;
 
 public class ODBScrapper{
 
-	public static void main (String[] args)
-	{
-		// jsoup sample.  parse html from string
-        	// and read out some nodes from the tree
+	/* jsoup sample.  parse html from string
+	 * and read out some nodes from the tree
+	 */
+	static void example1(){
 		String html1 = "<html><head><title>First parse</title></head>"
-				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
-        String html2 = "<html><head><title>First parse</title></head>"
-				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
-        String html = html1;
+				+ "<body><p>Parsed HTML into a doc.</p></body></html>";
+		String html2 = "<html><head><title>First parse</title></head>"
+				+ "<body><p>Parsed HTML into a doc.</p></body></html>";
+		String html = html1;
 		Document doc = Jsoup.parse(html);
 		System.out.println("Hello World");
 		//for (Node child:doc.childNodes()){
 		//	System.out.println(child.toString());
 		//}
-        System.out.println(doc.childNode(0).toString());
-        System.out.println(doc.childNode(0).childNodes().size());
-        System.out.println(doc.childNode(0).childNode(1).toString());
-        
-       	// Get HTML from string URL
-       	String URLstr = "http://www-inst.eecs.berkeley.edu/~cs164/fa13/";
+		System.out.println(doc.childNode(0).toString());
+		System.out.println(doc.childNode(0).childNodes().size());
+		System.out.println(doc.childNode(0).childNode(1).toString());
+	}
+	
+	/* Get HTML from string URL
+	 */
+	static void example2(){
+		String URLstr = "http://www-inst.eecs.berkeley.edu/~cs164/fa13/";
        	Document doc2;
         
        	try{
@@ -34,6 +38,18 @@ public class ODBScrapper{
             System.out.println("couldn't connect to " + URLstr);
         }
         
+	}
+	
+	/* Try ODBGetter methods
+	 */
+	static void ODBGetterExamples(){
+	
+	}
+	
+	public static void main (String[] args)
+	{
+		//example1();
+		//example2();
 	}
 	
 }
